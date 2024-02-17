bdf0c0e372121492121651af0f80a29950ed3660
AndyOoh <andy.oeee@gmail.com>
2024-02-17 17:23:04 +0700

    fix tsconfig

---
+++ src/js-new-features/js_2023.ts
@@ -0,0 +1,13 @@
+const inventory: any = [
+    { name: "asparagus", type: "vegetables", quantity: 5 },
+    { name: "bananas", type: "fruit", quantity: 0 },
+    { name: "goat", type: "meat", quantity: 23 },
+    { name: "cherries", type: "fruit", quantity: 5 },
+    { name: "fish", type: "meat", quantity: 22 },
+  ];
+
+  
+  await Promise.resolve(console.log('Hello World'));
+  //   const result = Object.groupBy()
+  
+  export {}
