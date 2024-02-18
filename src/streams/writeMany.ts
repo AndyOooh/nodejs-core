a754c518c00d0610bcfcaa88e60efde1660f72bf
AndyOoh <andy.oeee@gmail.com>
2024-02-18 23:11:23 +0700

    add cb versions

--- src/streams/writeMany.ts
+++ src/streams/writeMany.ts
@@ -1,29 +1,11 @@
 import { open } from 'node:fs/promises';
+import fs from 'node:fs';
 
-const writeToFile = async (path: string, data: any) => {
-  try {
-    const fileHandler = await open(path, 'a');
-    // await fileHandler.write(data + '\n');
-    await fileHandler.write(data);
-    await fileHandler.close();
-  } catch (error) {
-    console.log('🚀 File not found. writeToFile error:', error);
-  }
-};
-
-const main = async () => {
-  console.time('writeToFile');
-  const arr = Array.from({ length: 1e5 }, (v, i) => i);
-  await Promise.all(
-    arr.map(async (ele: any) => {
-      const path = __dirname + `/test.txt`;
-      await writeToFile(path, ele);
-    })
-  );
-  console.timeEnd('writeToFile');
-};
-
-const mainNew = async () => {
+/*
+ * Promise API
+ * Exec time: 22s
+ */
+const mainAsync = async () => {
   console.time('writeToFile');
   const fileHandler = await open(__dirname + '/test.txt', 'a');
   for (let i = 0; i < 1e5; i++) {
@@ -33,6 +15,18 @@ const mainNew = async () => {
   await fileHandler.close();
   console.timeEnd('writeToFile');
 };
+// mainAsync();
 
-mainNew();
-// main();
+/*
+ * Callback API
+ * Exec time: 20.4s
+ */
+fs.open(__dirname + '/test.txt', 'a', (err, fd) => {
+  console.time('writeToFile');
+  for (let i = 0; i < 1e5; i++) {
+    fs.write(fd, i + ' ', err => {
+      if (err) console.log('error writing to file: ', err);
+    });
+  }
+  console.timeEnd('writeToFile');
+});
