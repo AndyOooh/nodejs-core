796ba894f45c8b282b8b5d1a3cf0400a9540375a
AndyOoh <andy.oeee@gmail.com>
2024-02-18 20:47:01 +0700

    mainNew

--- src/streams/writeMany.ts
+++ src/streams/writeMany.ts
@@ -3,7 +3,8 @@ import { open } from 'node:fs/promises';
 const writeToFile = async (path: string, data: any) => {
   try {
     const fileHandler = await open(path, 'a');
-    await fileHandler.write(data + '\n');
+    // await fileHandler.write(data + '\n');
+    await fileHandler.write(data);
     await fileHandler.close();
   } catch (error) {
     console.log('🚀 File not found. writeToFile error:', error);
@@ -11,15 +12,27 @@ const writeToFile = async (path: string, data: any) => {
 };
 
 const main = async () => {
+  console.time('writeToFile');
   const arr = Array.from({ length: 1e5 }, (v, i) => i);
-  console.log('before');
   await Promise.all(
     arr.map(async (ele: any) => {
       const path = __dirname + `/test.txt`;
       await writeToFile(path, ele);
     })
   );
-  console.log('after');
+  console.timeEnd('writeToFile');
 };
 
-main();
+const mainNew = async () => {
+  console.time('writeToFile');
+  const fileHandler = await open(__dirname + '/test.txt', 'a');
+  for (let i = 0; i < 1e5; i++) {
+    // await fileHandler.write(i + '\n');
+    await fileHandler.write(i + ' ');
+  }
+  await fileHandler.close();
+  console.timeEnd('writeToFile');
+};
+
+mainNew();
+// main();
