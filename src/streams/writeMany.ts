8240c0e8de6aae2321a6d179f7f02c1ba5e9cfde
AndyOoh <andy.oeee@gmail.com>
2024-03-02 16:23:34 +0700

    add test results

--- src/streams/writeMany.ts
+++ src/streams/writeMany.ts
@@ -3,21 +3,26 @@ import fs from 'node:fs';
 
 /*
  * Promise API
- * Exec time: 22s
+ * 100k exec time: 15s
+ * 1M exec time: 3m 14s
  */
-// const mainAsync = async () => {
-//   console.time('writeToFile');
-//   const fileHandler = await open(__dirname + '/test.txt', 'a');
-//   for (let i = 0; i < 1e5; i++) {
-//     // await fileHandler.write(i + '\n');
-//     await fileHandler.write(i + ' ');
-//   }
-//   await fileHandler.close();
-//   console.timeEnd('writeToFile');
-//   console.log('mainAsync done 1')
-// };
-// mainAsync();
+const mainAsync = async () => {
+  console.time('writeToFile');
+  const fileHandler = await open(__dirname + '/test.txt', 'a');
+  for (let i = 0; i < 1e6; i++) {
+    // await fileHandler.write(i + '\n');
+    await fileHandler.write(i + ' ');
+  }
+  await fileHandler.close();
+  console.timeEnd('writeToFile');
+  console.log('mainAsync done 1');
+};
+mainAsync();
 
+/*
+ * Promise API IIF
+ * 100k exec time: 16s
+ */
 // (async () => {
 //   console.time('writeToFile');
 //   const fileHandler = await open(__dirname + '/test.txt', 'w');
@@ -30,18 +35,21 @@ import fs from 'node:fs';
 // })();
 
 /*
- * Callback API
- * Exec time: 0.375s
-//  */
-fs.open(__dirname + '/test.txt', 'a', (err, fd) => {
-  console.time('writeToFile');
-  for (let i = 0; i < 1e5; i++) {
-    // fs.writeSync(fd, i + ' ');
+ * Callback API / sync API (switch between them)
+ * 100k CB exec time: 0.3 - 0.4 s
+ * 100k Sync exec time: 0.3 - 0.4 s
+ * 1M CB exec time: 4 - 5 s
+ * 1M Sync exec time: 3 - 4 s
+ */
+// fs.open(__dirname + '/test.txt', 'a', (err, fd) => {
+//   console.time('writeToFile');
+//   for (let i = 0; i < 1e6; i++) {
+//     // fs.writeSync(fd, i + ' ');
 
-    /* Gets order wrong. Event loop cant keep track? */
-    fs.write(fd, i + ' ', err => {
-      if (err) console.log('error writing to file: ', err);
-    });
-  }
-  console.timeEnd('writeToFile');
-});
+//     /* Gets order wrong. Event loop cant keep track? */
+//     fs.write(fd, i + ' ', err => {
+//       if (err) console.log('error writing to file: ', err);
+//     });
+//   }
+//   console.timeEnd('writeToFile');
+// });
